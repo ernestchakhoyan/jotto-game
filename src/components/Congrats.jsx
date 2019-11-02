64899da16db789f3881b1cd3c2a36e77e593e908
ernest <echakhoyan@bluip.com>
2019-11-02 13:45:51 +0400

    Added prop types to congrats component

--- src/components/Congrats.jsx
+++ src/components/Congrats.jsx
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Congrats extends Component {
     render() {
-        const {success} = this.props;
+        const { success } = this.props;
         return (
             <div data-test="congrats-component">
-               {success ? (
-                   <div data-test="congrats-message">
-                       Congratulation! You guessed the word!
-                   </div>
-               ) : (
-                   <div data-test="congrats-no-message"/>
-               )}
+                {success ? (
+                    <div data-test="congrats-message">
+                        Congratulation! You guessed the word!
+                    </div>
+                ) : (
+                    <div data-test="congrats-no-message" />
+                )}
             </div>
         );
     }
 }
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired
+};
 
 export default Congrats;
--- src/components/Congrats.test.jsx
+++ src/components/Congrats.test.jsx
@@ -1,30 +1,43 @@
 import React from "react";
-import Enzyme , {shallow} from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
+
 import Congrats from "./Congrats";
-import { findByTestAttr } from "../test-utils/testUtil";
+
+import { checkProps, findByTestAttr } from "../test-utils/testUtil";
+
 Enzyme.configure({
     adapter: new EnzymeAdapter()
 });
 
-const setup = (props) => {
-    return shallow(<Congrats {...props}/>)
+const defaultPropTypes = {
+    success: false
 };
 
-it("> should render component" ,() => {
-    const wrapper = setup();
+const setup = (props) => {
+    const setupProps = {...defaultPropTypes, ...props};
+    return shallow(<Congrats {...setupProps} />);
+};
+
+it("> should render component", () => {
+    const wrapper = setup({ success: false });
     const congratsComponent = findByTestAttr(wrapper, "congrats-component");
     expect(congratsComponent.length).toBe(1);
 });
 
 it("> renders no text when `success` prop is false", () => {
-    const wrapper = setup({success: false});
+    const wrapper = setup({ success: false });
     const noTextComponent = findByTestAttr(wrapper, "congrats-no-message");
     expect(noTextComponent.text()).toBe("");
 });
 
 it("> renders non-empty congrats message when `success` prop is true", () => {
-    const wrapper = setup({success: true});
+    const wrapper = setup({ success: true });
     const noTextComponent = findByTestAttr(wrapper, "congrats-message");
     expect(noTextComponent.text().length).not.toBe(0);
+});
+
+it("> should not throw an error with expected props", () => {
+    const expectedProps = { success: false };
+    checkProps(Congrats, expectedProps);
 });
