d179f8c056cd176a1c24b00e9abd4736bdbbfd52
ernest <echakhoyan@bluip.com>
2019-11-02 18:37:00 +0400

    Guessed words are not  empty testing

--- src/components/GuessedWords/GuessedWords.jsx
+++ src/components/GuessedWords/GuessedWords.jsx
@@ -3,15 +3,39 @@ import * as PropTypes from "prop-types";
 
 function GuessedWords(props) {
     let contents;
-    if(props.guessedWords.length === 0){
+    if (props.guessedWords.length === 0) {
         contents = (
             <span data-test="guessed-instructions">
                 Try to guess the secret word!
             </span>
-        )
+        );
+    } else {
+        const guessedWordRows = props.guessedWords.map((word, index) => (
+            <tr data-test="guessed-word" key={index}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        ));
+
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed words</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Guess</th>
+                        <th>Matching Letters</th>
+                    </tr>
+                    </thead>
+                </table>
+                <tbody>
+                {guessedWordRows}
+                </tbody>
+            </div>
+        );
     }
     return (
-        <div data-test="component-guessed-word">
+        <div data-test="component-guessed-words">
             {contents}
         </div>
     );
@@ -19,10 +43,10 @@ function GuessedWords(props) {
 
 GuessedWords.propTypes = {
     guessedWords: PropTypes.arrayOf(
-       PropTypes.shape({
-           guessedWord: PropTypes.string.isRequired,
-           letterMatchCount: PropTypes.number.isRequired
-       })
+        PropTypes.shape({
+            guessedWord: PropTypes.string.isRequired,
+            letterMatchCount: PropTypes.number.isRequired
+        })
     ).isRequired
 };
 
--- src/components/GuessedWords/GuessedWords.test.js
+++ src/components/GuessedWords/GuessedWords.test.js
@@ -1,30 +1,30 @@
 import React from "react";
-import {shallow} from "enzyme";
+import { shallow } from "enzyme";
 import { checkProps, findByTestAttr } from "../../test-utils/testUtil";
 import "./GuessedWords";
 import GuessedWords from "./GuessedWords";
 
 const defaultProps = {
-    guessedWords: [{guessedWord: "train", letterMatchCount: 3}]
+    guessedWords: [ { guessedWord: "train", letterMatchCount: 3 } ]
 };
 
 const setup = (props = {}, state = null) => {
-    const setupProps = {...defaultProps, ...props};
-    return shallow(<GuessedWords {...setupProps}/>)
+    const setupProps = { ...defaultProps, ...props };
+    return shallow(<GuessedWords {...setupProps} />);
 };
 
 it("should not throw an error on checking props", () => {
-    checkProps(GuessedWords, defaultProps)
+    checkProps(GuessedWords, defaultProps);
 });
 
 describe("> If there are no words guessed", () => {
     let wrapper;
 
-    beforeEach(() =>{
-        wrapper = setup({guessedWords: []});
+    beforeEach(() => {
+        wrapper = setup({ guessedWords: [] });
     });
     it("should renders without error", () => {
-        const component = findByTestAttr(wrapper, "component-guessed-word");
+        const component = findByTestAttr(wrapper, "component-guessed-words");
         expect(component.length).toBe(1);
     });
 
@@ -35,5 +35,30 @@ describe("> If there are no words guessed", () => {
 });
 
 describe("> If there are words guessed", () => {
-    
+    let wrapper;
+
+    const guessedWords = [
+        { guessedWord: "train", letterMatchCount: 3 },
+        { guessedWord: "agile", letterMatchCount: 1 },
+        { guessedWord: "party", letterMatchCount: 5 },
+    ];
+
+    beforeEach(function () {
+        wrapper = setup({ guessedWords});
+    });
+
+    it("should renders without error", () => {
+        const component = findByTestAttr(wrapper, "component-guessed-words");
+        expect(component.length).toBe(1);
+    });
+
+    it("should renders 'guessed words' section", () => {
+        const guessedWordsNode = findByTestAttr(wrapper, "guessed-words");
+        expect(guessedWordsNode.length).toBe(1);
+    });
+
+    it("should renders correct number of guessed words", () => {
+        const guessedWordNode = findByTestAttr(wrapper, "guessed-word");
+        expect(guessedWordNode.length).toBe(guessedWords.length);
+    });
 });
