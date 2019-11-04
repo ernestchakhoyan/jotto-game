ba7ebacfd46187755f7772327569b06245119a44
ernest <echakhoyan@bluip.com>
2019-11-04 19:19:16 +0400

    Moxios testing

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,5 +1,30 @@
-import { actionTypes } from "./index";
+import moxios from "moxios";
+import { storeFactory } from "../test-utils/testUtil";
+import {getSecretWord} from "./index";
 
-it("should ", () => {
-    
-}); 
+describe("> getSecretWord action creator", () => {
+    beforeEach(function () {
+        moxios.install();
+    });
+    afterEach(function () {
+        moxios.uninstall();
+    });
+    it("should adds response word to state", () => {
+        const secretWord = "party";
+        const store = storeFactory();
+
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status: 200,
+                response: secretWord
+            });
+        });
+
+        return store.dispatch(getSecretWord())
+            .then(() => {
+                const newState = store.getState();
+                expect(newState.secretWord).toBe(secretWord);
+            })
+    });
+});
