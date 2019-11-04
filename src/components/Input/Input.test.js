cf39b1158f90e25b5e2147230b9a5dd747129ab8
ernest <echakhoyan@bluip.com>
2019-11-04 16:26:11 +0400

    Input testing

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -36,13 +36,26 @@ describe("> render", () => {
     });
 
     describe("> word has been guessed", () => {
+        let wrapper;
+
+        beforeEach(function () {
+            const  initialState = {success: true};
+            wrapper = setup(initialState);
+        });
+
+        it("should render component without errors", () => {
+            const component = findByTestAttr(wrapper, "component-input");
+            expect(component.length).toBe(1);
+        });
 
         it("should not renders input box", () => {
-
+            const component = findByTestAttr(wrapper, "input-box");
+            expect(component.length).toBe(0);
         });
 
         it("should not renders submit button", () => {
-
+            const component = findByTestAttr(wrapper, "submit-btn");
+            expect(component.length).toBe(0);
         });
     });
 });
